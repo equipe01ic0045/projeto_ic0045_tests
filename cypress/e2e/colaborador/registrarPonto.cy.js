548dcd4c71d3edc6222bf6d431308a1229e260dc
Thiago Coutinho Sousa Silva <tcoutinhossilva@gmail.com>
2023-09-22 19:25:21 -0300

    feature:projeto-basico

---
+++ cypress/e2e/colaborador/registrarPonto.cy.js
@@ -0,0 +1,18 @@
+describe('Registrar ponto', () => {
+    beforeEach(() => {
+      cy.visit('')
+    })
+  
+    it('Registrar horario de entrada', () => {
+
+    })
+    it('Registrar horario de saida para o almoço', () => {
+
+    })
+    it('Registrar horario de volta do almoço', () => {
+
+    })
+    it('Registrar horario de saída', () => {
+
+    })
+})
