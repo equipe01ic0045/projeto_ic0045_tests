acc683d470140e3ee8edf74fd3c25e9dc21835cd
Thiago Coutinho Sousa Silva <tcoutinhossilva@gmail.com>
2023-09-24 13:17:12 -0300

    feature:tests-architecture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,40 +4,23 @@ describe('Login', () => {
         cy.visit('http://localhost:4400/')
     })
 
-    context('Cadastro', () => {
-        it.only('Cadastrar novo usuário', () => {
-            //ir para a tela de cadastro
-            cy.get('a > .chakra-text').click()
-            cy.url().should('include', '/registration')
-    
-            //cadastrar usuário
-            cy.get('[placeholder="email"]').type("test@test.com.br")
-            cy.get('[placeholder="confirme o email"]').type("test@test.com.br")
-            cy.get('[placeholder="senha"]').type("admin")
-            cy.get('[placeholder="confirme a senha "]').type("admin")
-            cy.get('button').click()
-            //Verificar mensagem de cadastro
-            cy.get('#toast-1', {timeout:3000}).should('be.visible').invoke('text').should('contains', 'registro completo')
-        })
-    });
-
     context('Login', () => {
         it.only('Logar com usuário válido', () => {
-            //logar como um usuário válido
-            cy.get('input[name="email"]').type("test@test.com.br")
-            cy.get('input[name="password"]').type("admin")
-            cy.get('button').click()
-            //Verificar mensagem de login
-            cy.get('#toast-1', {timeout:3000}).should('be.visible').invoke('text').should('contains', 'login falhou')
+            cy.fixture('login/loginFixtures.json').then(dados => {
+                //logar como um usuário válido
+                cy.login(dados.email, dados.senha)
+                //Verificar mensagem de login
+                cy.get('#toast-1', {timeout:3000}).should('be.visible').invoke('text').should('contains', 'login falhou')
+            })
         })
     
         it.only('Tentar logar com usuário inválido', () => {
-            //logar como um usuário inválido
-            cy.get('input[name="email"]').type("invalido@test.com.br")
-            cy.get('input[name="password"]').type("invalido")
-            cy.get('button').click()
-            //Verificar mensagem de erro
-            cy.get('#toast-1', {timeout:3000}).should('be.visible').invoke('text').should('contains', 'login falhou')
+            cy.fixture('login/loginFixtures.json').then(dados => {
+                //logar como um usuário inválido
+                cy.login(dados.email, dados.senha)
+                //Verificar mensagem de erro
+                cy.get('#toast-1', {timeout:3000}).should('be.visible').invoke('text').should('contains', 'login falhou')
+            })
         })
     })
 })
